8664e0ed50a6f227da17e82d628aa40a50b262de
jiahuixing <jiahuixing@gmail.com>
2014-05-05 20:44:14 +0800

    init

---
+++ duokanbox.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python -u
+# -*- coding: utf-8 -*-
+__author__ = 'jiahuixing'
+
+from xml.etree import ElementTree as ET
+
+
+class DuokanBox:
+    def __init__(self):
+        pass
+
+    def get_sites(self):
+        pass
